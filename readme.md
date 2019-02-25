883d25751023273f0e7a66724b7e46672660b00f
tianbo <tianbo@xiaomi.com>
2019-02-25 11:08:41 +0800

    updated stylesheet

--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-|Title|Difficulty|Submissions|Note|
-|---|---|---|---|
-[999. Available Captures for Rook](https://leetcode.com/problems/available-captures-for-rook/)|Easy|[solution](https://github.com/zybotian/leetcode/blob/master/src/main/java/array/NumRookCaptures.java)|array|
+|Title|Submissions|Difficulty|Note|
+|------|------|------|------|
+[999. Available Captures for Rook](https://leetcode.com/problems/available-captures-for-rook/)|[solution](https://github.com/zybotian/leetcode/blob/master/src/main/java/array/NumRookCaptures.java)|Easy|array|
--- src/main/java/array/NumRookCaptures.java
+++ src/main/java/array/NumRookCaptures.java
@@ -6,6 +6,69 @@ package array;
  */
 public class NumRookCaptures {
 
+    static class Solution {
+        public int numRookCaptures(char[][] board) {
+            int ri = 0, rj = 0;
+
+            // 先找到R的位置
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    if (board[i][j] == 'R') {
+                        ri = i;
+                        rj = j;
+                        break;
+                    }
+                }
+            }
+
+            int result = 0;
+
+            // up, 往上寻找是否有'p'
+            for (int i = ri - 1; i >= 0; i--) {
+                if (board[i][rj] == 'p') {
+                    result++;
+                    break;
+                }
+                if (board[i][rj] == 'B') {
+                    break;
+                }
+            }
+            // down, 往下寻找是否有'p'
+            for (int i = ri + 1; i < 8; i++) {
+                if (board[i][rj] == 'p') {
+                    result++;
+                    break;
+                }
+                if (board[i][rj] == 'B') {
+                    break;
+                }
+            }
+
+            // left, 往左寻找是否有'p'
+            for (int j = rj - 1; j >= 0; j--) {
+                if (board[ri][j] == 'p') {
+                    result++;
+                    break;
+                }
+                if (board[ri][j] == 'B') {
+                    break;
+                }
+            }
+            // right, 往右寻找是否有'p'
+            for (int j = rj + 1; j < 8; j++) {
+                if (board[ri][j] == 'p') {
+                    result++;
+                    break;
+                }
+                if (board[ri][j] == 'B') {
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         char[][] arr1 = {{'.', '.', '.', '.', '.', '.', '.', '.'},
@@ -27,67 +90,6 @@ public class NumRookCaptures {
                 {'.', '.', '.', '.', '.', '.', '.', '.'},
                 {'.', '.', '.', '.', '.', '.', '.', '.'}};
         System.out.println(solution.numRookCaptures(arr2));
-
     }
 
-    static class Solution {
-        public int numRookCaptures(char[][] board) {
-            int ri = 0, rj = 0;
-            for (int i = 0; i < 8; i++) {
-                for (int j = 0; j < 8; j++) {
-                    if (board[i][j] == 'R') {
-                        ri = i;
-                        rj = j;
-                        break;
-                    }
-                }
-            }
-
-            int result = 0;
-
-            // up
-            for (int i = ri - 1; i >= 0; i--) {
-                if (board[i][rj] == 'p') {
-                    result++;
-                    break;
-                }
-                if (board[i][rj] == 'B') {
-                    break;
-                }
-            }
-            // down
-            for (int i = ri + 1; i < 8; i++) {
-                if (board[i][rj] == 'p') {
-                    result++;
-                    break;
-                }
-                if (board[i][rj] == 'B') {
-                    break;
-                }
-            }
-
-            // left
-            for (int j = rj - 1; j >= 0; j--) {
-                if (board[ri][j] == 'p') {
-                    result++;
-                    break;
-                }
-                if (board[ri][j] == 'B') {
-                    break;
-                }
-            }
-            // right
-            for (int j = rj + 1; j < 8; j++) {
-                if (board[ri][j] == 'p') {
-                    result++;
-                    break;
-                }
-                if (board[ri][j] == 'B') {
-                    break;
-                }
-            }
-
-            return result;
-        }
-    }
 }
