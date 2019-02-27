393b44f87e303cdf5029872d95feb07e9b4665e8
tianbo <tianbo@xiaomi.com>
2019-02-27 16:49:33 +0800

    ac 763

--- solution/two_pointers.md
+++ solution/two_pointers.md
@@ -1,3 +1,4 @@
 |Title|Submissions|Difficulty|Note|
 |------|------|------|------|
+[763. Partition Labels](https://leetcode.com/problems/partition-labels/)|[solution](https://github.com/zybotian/leetcode/blob/master/src/main/java/twopointers/PartitionLabels.java)|Easy|two pointers, greedy|
 [925. Long Pressed Name](https://leetcode.com/problems/long-pressed-name/)|[solution](https://github.com/zybotian/leetcode/blob/master/src/main/java/twopointers/IsLongPressedName.java)|Easy|string, two pointers|
---
+++ src/main/java/twopointers/PartitionLabels.java
@@ -0,0 +1,71 @@
+package twopointers;
+
+import java.util.*;
+
+/**
+ * @author tianbo
+ * @date 2019-02-27
+ */
+public class PartitionLabels {
+    public static void main(String[] args) {
+        PartitionLabels test = new PartitionLabels();
+        Solution solution = test.new Solution();
+        System.out.println(solution.partitionLabels("ababcbacadefegdehijhklij"));
+        System.out.println(solution.partitionLabels("caedbdedda"));
+        System.out.println(solution.partitionLabels("qiejxqfnqceocmy"));
+    }
+
+    class Solution {
+        public List<Integer> partitionLabels(String S) {
+            char[] chars = S.toCharArray();
+            Map<Character, Pair> map = new TreeMap<>();
+            for (int i = 0; i < chars.length; i++) {
+                char charAt = chars[i];
+                Pair pair = map.get(charAt);
+                if (pair == null) {
+                    pair = new Pair(i, i);
+                    map.put(charAt, pair);
+                } else {
+                    pair.last = i;
+                }
+            }
+
+            List<Integer> result = new LinkedList<>();
+
+            int i = 0;
+            while (i < chars.length) {
+                // 第一个字符
+                char first = chars[i];
+                Pair pair = map.get(first);
+                // 第一个字符的起始下标区间
+                int start = pair.first, end = pair.last;
+                // 初始化的最小最大下标区间
+                int min = start, max = end;
+                for (int s = start; s <= end; s++) {
+                    Pair tmpPair = map.get(chars[s]);
+                    min = Math.min(min, tmpPair.first);
+                    if (max < tmpPair.last) {
+                        // end可能扩大
+                        end = tmpPair.last;
+                    }
+                    max = Math.max(max, tmpPair.last);
+                }
+                result.add(max + 1 - min);
+                i = max + 1;
+            }
+
+            return result;
+        }
+
+        class Pair {
+            int first;
+            int last;
+
+            public Pair(int first, int last) {
+                this.first = first;
+                this.last = last;
+            }
+
+        }
+    }
+}
